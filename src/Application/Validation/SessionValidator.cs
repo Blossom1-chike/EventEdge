339d6b000da6cc1504df5e4ea31ad1db2809bb1f
Chikezie Blossom <chinanuchikezie515@gmail.com>
2022-03-29 15:29:39 +0100

    Created a Session Entity and a Session Validation file

---
+++ src/Application/Validation/SessionValidator.cs
@@ -0,0 +1,18 @@
+﻿using Domain.Entities;
+using FluentValidation;
+
+namespace Application.Validation
+{
+    public class SessionValidator: AbstractValidator<Session>
+    {
+        public SessionValidator()
+        {
+            RuleFor(x => x.Title).NotEmpty().WithMessage("Session Title cannot be empty");
+            RuleFor(x => x.Start_Time).NotEmpty().WithMessage("Start Time cannot be empty");
+            RuleFor(x => x.End_Time).NotEmpty().WithMessage("End Time cannot be empty");
+            RuleFor(x => x.Location_Type).NotEmpty().WithMessage("Location Type cannot be empty");
+            RuleFor(x => x.Day_Id).NotEmpty().WithMessage("A Session should have a Day");
+
+        }
+    }
+}
--- src/Domain/Entities/Session.cs
+++ src/Domain/Entities/Session.cs
@@ -1,13 +1,25 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using FluentValidation;
+
 
 namespace Domain.Entities
 {
     public class Session
     {
         public Guid Id { get; set; }
+        public string Title { get; set; }
+        public string Notes { get; set; }
+        public DateTime Start_Time { get; set; }
+        public DateTime End_Time { get; set; }
+        public Guid Day_Id { get; set; }
+        public string Location_Type { get; set; }
+        public string Venue_Name { get; set; }
+        public string First_Address { get; set; }
+        public string? Second_Address { get; set; }
+        public string City { get; set; }
+        public string State { get; set; }
+        public int PostalCode { get; set; }
+        public string Country { get; set; }
+        public Guid Link_Id { get; set; }
+        public IEnumerable<string> Links { get; set; }
     }
 }
